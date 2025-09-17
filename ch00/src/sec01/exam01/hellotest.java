e82c7c3659b56fa04deb3a2fedffad5c27222e03
sungharin <sungharin05@naver.com>
2025-09-17 13:08:09 +0900

    Create hellotest.java

---
+++ ch00/src/sec01/exam01/hellotest.java
@@ -0,0 +1,10 @@
+package sec01.exam01;
+
+public class hellotest {
+	   public static void main(String[] args) {
+		      // TODO Auto-generated method stub
+		         int x = 1;
+		         int y = 2;
+		         int result = x + y;
+		   }
+}
