dabd7c599c73fa392449048209730fd4a415325a
sungharin <sungharin05@naver.com>
2025-09-17 13:26:14 +0900

    Create typetest.java

---
+++ ch01/src/sec01/exam01/typetest.java
@@ -0,0 +1,24 @@
+package sec01.exam01;
+
+public class typetest {
+   public static void main(String[] args) {
+      int x = 1;
+      int y = 2;
+      int result = x + y;
+      System.out.println(result);
+      }
+
+   //실습
+      byte byteVal = 10;
+      int intVal = byteVal;
+      //byte byteVal = intVal;\
+         
+      char charVal = 65;
+      //char charVal = 'A';
+      //int intVall = charVal;
+      //byte byteVall = 65;
+      //int intVal2 = vyteVall;
+      //char charVal2 = byteVall;
+      System.out.println(result);
+     }
+
