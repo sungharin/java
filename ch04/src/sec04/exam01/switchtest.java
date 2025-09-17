d36e5e1d2f1e065020f1885eeb4d670fce66081d
sungharin <sungharin05@naver.com>
2025-09-17 13:33:46 +0900

    Create switchtest.java

---
+++ ch04/src/sec04/exam01/switchtest.java
@@ -0,0 +1,22 @@
+package sec04.exam01;
+
+public class switchtest {
+	public static void main(String[] args) {
+		int val = 1;
+//  String val = "Mon";
+		switch(val) {
+		case 1:
+			System.out.println("code 1");
+			break;
+		case 2:
+			System.out.println("code 2");
+			break;
+		case 3:
+			System.out.println("code 3");
+			break;
+		default:
+			System.out.println("No code");
+
+		}
+	}
+}
