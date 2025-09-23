06eb1472f9de94f14e39c8b77f15c6e7cf20e188
sungharin <sungharin05@naver.com>
2025-09-23 10:12:36 +0900

    ch05

---
+++ ch05/src/sec01/exam1/varaveltest.java
@@ -0,0 +1,18 @@
+package sec01.exam1;
+
+public class varaveltest {
+
+  public static void main(String[] args) {
+	  int a = 0;
+	  int b = a;
+	  
+	  String str1 = null;
+	  String str2 = strl;
+	  
+	  System.out.println("Hi.");
+	  
+	  int[] intArray = null;
+	  intArray[0] = 10;	  
+  }
+	  
+  }
