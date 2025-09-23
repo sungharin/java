0b5248e7e7a577ddbae50217eb430e41c6cef3ce
sungharin <sungharin05@naver.com>
2025-09-23 10:43:46 +0900

    Create ArrayTest.java

---
+++ ch05/src/sec02/exam01/ArrayTest.java
@@ -0,0 +1,13 @@
+package sec02.exam01;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		int[] intArray = new int[10];
+		
+		for(int i=0; i<10; i++) {
+			System.out.println(intArray[i]);
+			
+		}
+		}
+}
