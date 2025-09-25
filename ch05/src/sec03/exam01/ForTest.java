2ce94192be9fff0f9deeef1790bfa3c632e8a3a8
sungharin <sungharin05@naver.com>
2025-09-25 14:41:51 +0900

    ch05

---
+++ ch05/src/sec03/exam01/ForTest.java
@@ -0,0 +1,16 @@
+package sec03.exam01;
+
+public class ForTest {
+
+	public static void main(String[]args) {
+		int[] scores = {91, 75, 84, 83, 87};
+		
+		int sum = 0;
+//		for(int i =0; i<scores.length; i++) {
+//			sum += scores[i];
+//		}
+		
+		System.out.println("Sum : " + sum);
+	}
+}
+	
