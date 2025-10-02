13c9a5ab77e0cc0a66762129bc2fd5c37722bf49
sungharin <sungharin05@naver.com>
2025-10-02 14:45:26 +0900

    ch06

--- ch06/src/sec01/exam01/MainClass.java
+++ ch06/src/sec01/exam01/MainClass.java
@@ -2,9 +2,18 @@ package sec01.exam01;
 
 public class MainClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+   public static void main(String[] args) {
+      Student st1 = new Student();
+//      stl.SayHello();
+      car c = new car("Hyundai", 200);
+//      c.SetMaxSpeed(100);
+ //     c.maxSpeed = 200;
+  //    c.ShowInfo();
+      
+      calcul cal = new calcul();
+      int val = cal.add(1,  3);
+      double vald = cal.add(1.0, 2.0);
+      System.out.println("value" + val);
+   }
 
 }
--- ch06/src/sec01/exam01/calcul.java
+++ ch06/src/sec01/exam01/calcul.java
@@ -1,5 +1,12 @@
 package sec01.exam01;
 
 public class calcul {
-
+ int add(int a, int b)
+{
+	 return a+b;
+}
+ double add(double a, double b)
+ {
+	 return a+b;
+ }
 }
