e611c1f3aa71c03af32da51f5c3958d09113c0d5
sungharin <sungharin05@naver.com>
2025-11-17 20:22:21 +0900

    독학23

---
+++ 독학23/src/sec01/exam04/SupersonicAirplaneExample.java
@@ -0,0 +1,13 @@
+package sec01.exam04;
+
+public class SupersonicAirplaneExample {
+    public static void main(String[] args) {
+        SupersonicAirplane sa = new SupersonicAirplane();
+        sa.takeOff();
+        sa.flyMode = SupersonicAirplane.SUPERSONIC;
+        sa.fly();
+        sa.flyMode = SupersonicAirplane.NORMAL;
+        sa.fly();
+        sa.land();
+    }
+}
