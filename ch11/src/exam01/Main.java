ce6ea21b822bae82ea5c0478e1d9af77884d5472
sungharin <sungharin05@naver.com>
2025-11-20 14:48:13 +0900

    ch11

---
+++ ch11/src/exam01/Main.java
@@ -0,0 +1,23 @@
+package exam01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Object obj2 = new Object();
+		obj2 = obj1;
+		
+		if(obj1 == obj2)
+			System.out.println("Equal!!");
+		else
+			System.out.println("Not Equal!!");
+		
+
+		if(obj1.equals(obj2))
+			System.out.println("Equal!!");
+		else
+			System.out.println("Not Equal!!");
+		
+	}
+
+}
